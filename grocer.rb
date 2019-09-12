0b92515b7d61ad8c5e07ed564f2b527f063dfdcd
Li Xie <li_xie1992@hotmail.com>
2019-09-12 17:55:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def apply_coupons(cart, coupons)
   
   coupons.each do |coupon|
 
-    if cart.keys.include?(coupon[:item] 
+    if cart.keys.include?(coupon[:item] )
       if (cart[coupon[:item]][:count] >= coupon[:num])
       
         clearance_item_name = coupon[:item] + " W/COUPON"
