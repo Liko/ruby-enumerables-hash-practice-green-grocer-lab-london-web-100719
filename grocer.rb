77ff4c6c9419a6123e9dff9fa1e8f97883967cea
Li Xie <li_xie1992@hotmail.com>
2019-09-12 17:52:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -54,7 +54,10 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  discounted_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  #discounted_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  cart1 = consolidate_cart(cart)
+  cart2 = apply_coupons(cart1, coupons)
+  discounted_cart = apply_clearance(cart2)
   total = 0.0 
   
   discounted_cart.keys.each do |item|
