8665678b553902295994f9ab1b2508881e7efae9
Li Xie <li_xie1992@hotmail.com>
2019-09-12 15:42:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 def consolidate_cart(cart_array)
   cart_hash = {}
   
-  cart_array.each do |item|
+  cart_array.each do |i|
     #add each item to the cart_hash, and add a count key:value to the hash. if it is the first time, add it with quantity is 1. for each time, check if it is the first time or not, and if it is not the first time, simply update the count.
     cart_item = cart_array[i]
     cart_item_key = cart_item.keys
