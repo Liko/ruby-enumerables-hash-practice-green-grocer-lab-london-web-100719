1b066400a579e569ed3288f540587461fc87d129
Li Xie <li_xie1992@hotmail.com>
2019-09-12 17:36:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -54,7 +54,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = apply_coupons(consolidate_cart(cart), coupons)
+  cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
 end
 
 # coupons = [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
