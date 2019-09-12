6e4b9958ce0e85a525dd74a155ad1d958e4724ff
Li Xie <li_xie1992@hotmail.com>
2019-09-12 16:15:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,10 +2,12 @@ def consolidate_cart(cart)
   cart_hash = {}
   
   cart.each do |item|
-    if cart_hash[item.keys.first]
-      cart_hash[item.keys.first][:count] += 1 
+    item_name = item.keys.first 
+    
+    if cart_hash[item_name]
+      cart_hash[item_name][:count] += 1 
     else 
-      cart_hash[item.keys.first] = {
+      cart_hash[item_name] = {
         price: item.values.first[:price],
         clearance: item.values.first[:clearance],
         count: 1
@@ -22,7 +24,9 @@ def apply_coupons(cart, coupons)
   #with these items, check if the count in basket is at least equal to num in coupon, if so, apply the coupon, if not, check the next items
   
   coupons.each do |coupon|
-    puts coupon.values.first
+    puts coupon.values.first #AVOCADO
+    puts 
+    if coupon.values.f
   end 
 
   
