05dd1a05a2373e45b250d202fb1723f8d3dc6ab9
Li Xie <li_xie1992@hotmail.com>
2019-09-12 17:16:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def apply_coupons(cart, coupons)
     
 
 
-    if (cart.keys.include?(coupon[:item] && cart[coupon[:item]][:count] >= coupon[:num])
+    if (cart.keys.include?(coupon[:item]) && cart[coupon[:item]][:count] >= coupon[:num])
       
       adjusted_item = coupon[:item] + " W/COUPON"
       adjusted_num = (cart[coupon[:item]][:count] / coupon[:num]) * coupon[:num]
