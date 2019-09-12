43e29ff679b247a8128674b960743e38e171fa6f
Li Xie <li_xie1992@hotmail.com>
2019-09-12 16:51:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -32,7 +32,7 @@ def apply_coupons(cart, coupons)
       #apply coupon
       
       adjusted_item = coupon_item + " W/COUPON"
-      adjusted_price = coupon_cost / coupon_num
+      adjusted_cost = coupon_cost / coupon_num
       
       puts adjusted_item
       puts adjusted_price
