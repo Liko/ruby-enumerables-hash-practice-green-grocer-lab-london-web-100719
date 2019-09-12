b7961db708d80d6db741d9f954d57788ed755f1c
Li Xie <li_xie1992@hotmail.com>
2019-09-12 16:45:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,10 +28,7 @@ def apply_coupons(cart, coupons)
     coupon_num = coupon.values[1]
     coupon_cost = coupon.values[2]
     
-    
-    cart.each do |item|
-    
-      puts item.keys.first
+    puts cart[coupon_item]
     end 
     
     
